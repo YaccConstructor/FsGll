e122d7bd34957960bbb39288dcd93d43618034c7
Kirill Melentyev <melentyev.k@gmail.com>
2015-12-07 14:11:09 +0300

    extcalc

--- src/FsGll/Tests3.fs
+++ src/FsGll/Tests3.fs
@@ -6,25 +6,44 @@ open System.Diagnostics
 open FsGll.ExtCalcLexer
 
 let runExtCalc (inp) = 
+    let tok t = satisfy ((=)t)
     let expr = createParserForwardedToRef("expr")
-    let term = pipe3 factor additiveOp factor (fun a x b -> )
-    let var    = satisfy (function | INDENTIFIER _ -> true | _ -> false)
-    let eq     = satisfy (function | EQ -> true | _ -> false)
+    let var    = satisfy (function | IDENTIFIER _ -> true | _ -> false)
     
-    let assign = (var .>> eq) .>>. expr
-    let pgm = many1 assign .>>. expr |> (fun (l, a) -> l @ [a])
+    let num    = 
+        satisfy (function | INT _ -> true | _ -> false)
+        |>> (function | INT x -> x | _ -> failwith "impossible") 
+    
+    let additiveOp = satisfy (function | PLUS | MINUS -> true | _ -> false)
+
+    let multOp = 
+        satisfy (function | PLUS | MINUS -> true | _ -> false) 
+        |>> (function | PLUS -> (+) | MINUS -> (-) | _ -> failwith "impossible") 
+    
+    let factor = 
+            tok (SYM "(") >>. expr .>> tok (SYM ")") 
+        <|> num
+    
+    let term   = pipe3 factor multOp term (fun a x b -> (function | PLUS -> (+) | MINUS -> (-) | _ -> failwith "impossible") a b)
+    expr      := pipe3 term additiveOp expr (fun a x b -> x a b)
+    
+    
+    let assign = (var .>> tok EQ) .>>. expr
+    let pgm = many1 assign .>>. expr |>> (fun (l, a) -> l @ [a])
     let lexbuf = Microsoft.FSharp.Text.Lexing.LexBuffer<_>.FromString inp
+
     let toks = 
         Seq.initInfinite (fun _ -> FsGll.ExtCalcLexer.tokenize lexbuf) 
         |> Seq.cache
         |> Seq.takeWhile (function | EOF -> false | _ -> true)
         |> Seq.toList
         |> runParser pgm
+    ()
 
 let runExample() = 
     let res = runExtCalc " 0 + 1+ 2 * 8 -1 "
 
-
+    ()
 //    let sw = new Stopwatch()
 //    sw.Start()
 //    let a = badGrammar(90)
