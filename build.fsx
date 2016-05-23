6bd71c4dd3de848cb48b562826a41e4b263082c7
Kirill Melentyev <melentyev.k@gmail.com>
2016-05-23 18:34:10 +0300

    Bump version to 1.0

--- build.fsx
+++ build.fsx
@@ -52,7 +52,7 @@ let testAssemblies = "tests/**/bin/Release/*Tests*.dll"
 
 // Git configuration (used for publishing documentation in gh-pages branch)
 // The profile where the project is posted
-let gitOwner = "melentyev"
+let gitOwner = "YaccConstructor"
 let gitHome = "https://github.com/" + gitOwner
 
 // The name of the project on GitHub
